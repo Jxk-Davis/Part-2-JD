a44a841864ae7d12b66d9ef654c1de4940566e6b
Jxk-Davis <lovingpotion@gmail.com>
2024-01-31 11:21:59 -0500

    Task 2 COMPLETE

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -7,13 +7,20 @@ public class Plane : MonoBehaviour
     public List<Vector2> points;
     public float newPointThreshold = 0.2f;
     Vector2 lastPos;
+    Rigidbody2D rb2d;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    public float spe;
+    public AnimationCurve landing;
+    float landingTimer;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 1;
-        lineRenderer.SetPosition(0, transform.position);
+        rb2d = GetComponent<Rigidbody2D>();
+        ResetLineRenderer(transform.position);
+        //lineRenderer.positionCount = 1;
+        //lineRenderer.SetPosition(0, transform.position);
     }
 
     private void OnMouseDown()
@@ -24,8 +31,9 @@ public class Plane : MonoBehaviour
         points.Add(currentPos);
 
         //lines----------------------
-        lineRenderer.positionCount = 1;
-        lineRenderer.SetPosition(0, transform.position);
+        ResetLineRenderer(transform.position);
+        //lineRenderer.positionCount = 1;
+        //lineRenderer.SetPosition(0, transform.position);
     }
 
     private void OnMouseDrag()
@@ -39,5 +47,54 @@ public class Plane : MonoBehaviour
                 lastPos = currentPos;
             }
         //do something smart
+    }
+
+    private void FixedUpdate()
+    {
+        currentPosition = new Vector2(transform.position.x, transform.position.y);
+
+        if(points.Count > 0 )
+        {
+            Vector2 dir = points[0] - currentPosition; //??????????? gets the direction between 2 points?
+            float angle = Mathf.Atan2(dir.x, dir.y)* Mathf.Rad2Deg;
+            rb2d.rotation = -angle;
+        }
+
+        rb2d.MovePosition(rb2d.position + (Vector2)transform.up * spe * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            landingTimer += 0.1f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingTimer);
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, interpolation);
+
+            if (transform.localScale.x < 0.1)
+            {
+                Destroy(gameObject); //when the beat drops...
+            }
+        }
+
+        if(points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++) //write 1 to 0, 2 to 1, 3 to 2.... etc. turn 1 into the new zero
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));//bring value from ahead to here
+
+                }
+                lineRenderer.positionCount -= 1; //get rid of the now redundant final number
+            }
+        }
+    }
+    private void ResetLineRenderer(Vector3 position)
+    {
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, position);
     }
 }
