844f4b77091c310ebcd5f42739bfc1f5d0531fd9
Jxk-Davis <juliannaxkd@gmail.com>
2024-02-21 11:11:02 -0500

    save

---
+++ Assets/Week 7/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ontriggerenter, add 1 to score, reset position and velocity.
+}
